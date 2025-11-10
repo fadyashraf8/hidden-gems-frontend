10f3ef8ce3d8ed16a7c0fd6d31a7513b426e2e49
fadyashraf8 <fadyashraf909@gmail.com>
2025-11-10 23:02:27 +0200

    setup the project

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer.jsx";
+
+export default function Layout() {
+  return (
+    <>
+      <Navbar></Navbar>
+
+      <Outlet></Outlet>
+
+
+      <Footer></Footer>
+    </>
+  );
+}
